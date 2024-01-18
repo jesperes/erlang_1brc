39ec706aeb715cb7ace96864ff32bc39d79c6553
Lukas Larsson <lukas@erlang.org>
2024-01-18 09:38:52 +0100

    Add flow control

--- src/aggregate2.erl
+++ src/aggregate2.erl
@@ -23,7 +23,7 @@ aggregate_measurements(Filename, Opts) ->
     proplists:get_value(parallel, Opts, erlang:system_info(schedulers)),
   {ProcessorPids, AllPids} = start_processors(NumProcessors),
   {ok, Bin} = file:pread(FD, 0, BufSize),
-  read_chunks(FD, 0, byte_size(Bin), Bin, BufSize, ProcessorPids),
+  read_chunks(FD, 0, byte_size(Bin), Bin, BufSize, ProcessorPids, NumProcessors * 3),
   Now = erlang:monotonic_time(),
   logger:info(#{label => "All chunks read, waiting for processors to finish",
                 elapsed_secs => (Now - Start) / 1000_000_000.0}),
@@ -48,7 +48,12 @@ start_processors(NumProcs) ->
          [ProcessorPid|AllPids]}
     end, {[], []}, lists:seq(1, NumProcs)).
 
-read_chunks(FD, N, Offset, PrevChunk, BufSize, TargetPids) ->
+read_chunks(FD, N, Offset, PrevChunk, BufSize, TargetPids, 0) ->
+    receive
+        give_me_more ->
+            read_chunks(FD, N, Offset, PrevChunk, BufSize, TargetPids, 1)
+    end;
+read_chunks(FD, N, Offset, PrevChunk, BufSize, TargetPids, Outstanding) ->
   TargetPid = lists:nth((N rem length(TargetPids)) + 1, TargetPids),
   case file:pread(FD, Offset, BufSize) of
     {ok, Bin} ->
@@ -59,10 +64,10 @@ read_chunks(FD, N, Offset, PrevChunk, BufSize, TargetPids) ->
         [First, NextChunk] ->
           send_chunk([PrevChunk, First], TargetPid),
           %% sleep_if_target_pid_mql_too_long(TargetPid, 20),
-          read_chunks(FD, N + 1, Offset + Size, NextChunk, BufSize, TargetPids);
+          read_chunks(FD, N + 1, Offset + Size, NextChunk, BufSize, TargetPids, Outstanding - 1);
         [Chunk] ->
           send_chunk([Chunk], TargetPid),
-          read_chunks(FD, N + 1, Offset + Size, <<>>, BufSize, TargetPids)
+          read_chunks(FD, N + 1, Offset + Size, <<>>, BufSize, TargetPids, Outstanding - 1)
       end;
     eof ->
       %% Reached end of file, process the last chunk
@@ -86,6 +91,8 @@ wait_for_completion(Pids, Map) ->
                     sender => SenderPid,
                     result_size => maps:size(NewMap)}),
       wait_for_completion(Pids, merge_location_data(Map, NewMap));
+      give_me_more ->
+          wait_for_completion(Pids, Map);
     M ->
       logger:error(#{label => "Unexpected message", msg => M})
   end.
@@ -146,6 +153,7 @@ chunk_processor_loop(Pid) ->
       chunk_processor_loop(NewPid);
     {chunk, [Chunk]} ->
       process_station(Chunk),
+          Pid ! give_me_more,
       chunk_processor_loop(Pid);
     {chunk, [First, Second]} ->
           case process_station(First) of
@@ -156,6 +164,7 @@ chunk_processor_loop(Pid) ->
               Rest ->
                   process_station(<<Rest/binary, Second/binary>>)
           end,
+          Pid ! give_me_more,
       chunk_processor_loop(Pid);
     eof ->
       Map = maps:from_list(get()),
