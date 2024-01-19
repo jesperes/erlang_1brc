1b06484e0839be58d51b3543cbc50d6dda9272b5
Lukas Larsson <lukas@erlang.org>
2024-01-19 08:31:06 +0100

    Revert "Try to use prim_buffer"
    This reverts commit ac0d4569389d1f738bb4b99656b8b9356bc237ef.

--- src/aggregate2.erl
+++ src/aggregate2.erl
@@ -5,25 +5,22 @@
         , chunk_processor/0
         ]).
 
--compile({inline,[{process_temp,1},{process_line,2}]}).
+-compile({inline,[{process_temp,2},{process_line,3}]}).
 
 -include_lib("eunit/include/eunit.hrl").
 
 run([Filename]) ->
-%    logger:update_primary_config(#{ level => none }),
-%    dbg:tracer(),dbg:p(all,c),dbg:tpl(?MODULE,x),
-    aggregate_measurements(atom_to_list(Filename), [%{bufsize, 2 * 1024 * 1024}%, {parallel, 1}
-                                                    {bufsize, 10 * 1024}%, {parallel, 1}
-                                                   ]).
+    logger:update_primary_config(#{ level => none }),
+    aggregate_measurements(atom_to_list(Filename), [{bufsize, 2 * 1024 * 1240}]).
 
 aggregate_measurements(Filename, Opts) ->
   process_flag(trap_exit, true),
   Start = erlang:monotonic_time(),
   BufSize = proplists:get_value(bufsize, Opts),
   logger:info(#{bufsize => BufSize}),
+  {ok, FD} = file:open(Filename, [raw, read, binary]),
   NumProcessors =
     proplists:get_value(parallel, Opts, erlang:system_info(schedulers)),
-  {ok, FD} = file:open(Filename, [raw, read, binary]),
   {ProcessorPids, AllPids} = start_processors(NumProcessors),
   {ok, Bin} = file:pread(FD, 0, BufSize),
   read_chunks(FD, 0, byte_size(Bin), Bin, BufSize, ProcessorPids, NumProcessors * 3),
@@ -57,32 +54,30 @@ read_chunks(FD, N, Offset, PrevChunk, BufSize, TargetPids, 0) ->
             read_chunks(FD, N, Offset, PrevChunk, BufSize, TargetPids, 1)
     end;
 read_chunks(FD, N, Offset, PrevChunk, BufSize, TargetPids, Outstanding) ->
-    TargetPid = lists:nth((N rem length(TargetPids)) + 1, TargetPids),
-    case file:pread(FD, Offset, BufSize) of
-        {ok, Bin} ->
-            Size = byte_size(Bin),
-            %% Read chunks pair-wise and split them so that each processed
-            %% chunk is on an even newline boundary
-            case binary:split(Bin, <<"\n">>) of
-                [First, NextChunk] ->
-                    send_chunk([PrevChunk, First], TargetPid),
-                    %% sleep_if_target_pid_mql_too_long(TargetPid, 20),
-                    read_chunks(FD, N + 1, Offset + Size, NextChunk, BufSize, TargetPids, Outstanding - 1);
-                [Chunk] ->
-                    send_chunk([Chunk], TargetPid),
-                    read_chunks(FD, N + 1, Offset + Size, <<>>, BufSize, TargetPids, Outstanding - 1)
-            end;
-        eof ->
-            %% Reached end of file, process the last chunk
-            send_chunk([PrevChunk], TargetPid),
-            lists:foreach(fun(Pid) -> Pid ! eof end, TargetPids),
-            ok
-    end.
+  TargetPid = lists:nth((N rem length(TargetPids)) + 1, TargetPids),
+  case file:pread(FD, Offset, BufSize) of
+    {ok, Bin} ->
+      Size = byte_size(Bin),
+      %% Read chunks pair-wise and split them so that each processed
+      %% chunk is on an even newline boundary
+      case binary:split(Bin, <<"\n">>) of
+        [First, NextChunk] ->
+          send_chunk([PrevChunk, First], TargetPid),
+          %% sleep_if_target_pid_mql_too_long(TargetPid, 20),
+          read_chunks(FD, N + 1, Offset + Size, NextChunk, BufSize, TargetPids, Outstanding - 1);
+        [Chunk] ->
+          send_chunk([Chunk], TargetPid),
+          read_chunks(FD, N + 1, Offset + Size, <<>>, BufSize, TargetPids, Outstanding - 1)
+      end;
+    eof ->
+      %% Reached end of file, process the last chunk
+      send_chunk([PrevChunk], TargetPid),
+      lists:foreach(fun(Pid) -> Pid ! eof end, TargetPids),
+      ok
+  end.
 
 send_chunk(Chunk, TargetPid) ->
-    Buff = prim_buffer:new(),
-    prim_buffer:write(Buff, Chunk),
-    TargetPid ! {chunk, Buff}.
+  TargetPid ! {chunk, Chunk}.
 
 wait_for_completion([], Map) ->
   logger:info(#{label => "All subprocesses finished"}),
@@ -156,10 +151,21 @@ chunk_processor_loop(Pid) ->
   receive
     {result_pid, NewPid} ->
       chunk_processor_loop(NewPid);
-    {chunk, Buff} ->
-          process_line(Buff),
+    {chunk, [Chunk]} ->
+      process_station(Chunk),
           Pid ! give_me_more,
-          chunk_processor_loop(Pid);
+      chunk_processor_loop(Pid);
+    {chunk, [First, Second]} ->
+          case process_station(First) of
+              <<>> ->
+                  ok;
+              {Rest, Station} ->
+                  process_temp(<<Rest/binary, Second/binary>>, Station);
+              Rest ->
+                  process_station(<<Rest/binary, Second/binary>>)
+          end,
+          Pid ! give_me_more,
+      chunk_processor_loop(Pid);
     eof ->
       Map = maps:from_list(get()),
       Pid ! {result, self(), Map},
@@ -175,38 +181,30 @@ chunk_processor_loop(Pid) ->
 %% The line processor
 %%
 
-process_line(Buff) ->
-    case prim_buffer:find_byte_index(Buff, $;) of
-        {ok, StationIndex} ->
-            Station = prim_buffer:read(Buff, StationIndex),
-            prim_buffer:skip(Buff, 1),
-            case prim_buffer:find_byte_index(Buff, $\n) of
-                {ok, TempIndex} ->
-                    Temp = prim_buffer:read(Buff, TempIndex),
-                    prim_buffer:skip(Buff, 1),
-                    process_line(Station, process_temp(Temp)),
-                    process_line(Buff);
-                not_found ->
-                    Temp = prim_buffer:read(Buff, prim_buffer:size(Buff)),
-                    process_line(Station, process_temp(Temp))
-            end;
-        not_found ->
-            % <<>> = prim_buffer:read(Buff, prim_buffer:size(Buff)),
-            ok
-    end.
+process_station(Station) ->
+    process_station(Station, Station, 0).
+process_station(Bin, <<";", Rest/bitstring>>, Cnt) ->
+    <<Station:Cnt/binary, _/bitstring>> = Bin,
+    process_temp(Rest, Station);
+process_station(Bin, <<_:8, Rest/bitstring>>, Cnt) ->
+    process_station(Bin, Rest, Cnt + 1);
+process_station(Bin, _, _Cnt) ->
+    Bin.
 
 -define(TO_NUM(C), (C - $0)).
 
-process_temp(<<$-, A, B, $., C>>) ->
-    -1 * (?TO_NUM(A) * 100 + ?TO_NUM(B) * 10 + ?TO_NUM(C));
-process_temp(<<$-, B, $., C>>) ->
-    -1 * (?TO_NUM(B) * 10 + ?TO_NUM(C));
-process_temp(<<A, B, $., C>>) ->
-    ?TO_NUM(A) * 100 + ?TO_NUM(B) * 10 + ?TO_NUM(C);
-process_temp(<<B, $., C>>) ->
-    ?TO_NUM(B) * 10 + ?TO_NUM(C).
+process_temp(<<$-, A, B, $., C, Rest/binary>>, Station) ->
+    process_line(Rest, Station, -1 * (?TO_NUM(A) * 100 + ?TO_NUM(B) * 10 + ?TO_NUM(C)));
+process_temp(<<$-, B, $., C, Rest/binary>>, Station) ->
+    process_line(Rest, Station, -1 * (?TO_NUM(B) * 10 + ?TO_NUM(C)));
+process_temp(<<A, B, $., C, Rest/binary>>, Station) ->
+    process_line(Rest, Station, ?TO_NUM(A) * 100 + ?TO_NUM(B) * 10 + ?TO_NUM(C));
+process_temp(<<B, $., C, Rest/binary>>, Station) ->
+    process_line(Rest, Station, ?TO_NUM(B) * 10 + ?TO_NUM(C));
+process_temp(Rest, Station) ->
+    {Rest, Station}.
 
-process_line(Station, Temp) ->
+process_line(Rest, Station, Temp) ->
     case get(Station) of
         undefined ->
             put(Station, { Temp % min
@@ -222,4 +220,9 @@ process_line(Station, Temp) ->
                          , OldSum + Temp
                          })
 
+    end,
+    case Rest of
+        <<>> -> <<>>;
+        <<"\n",NextStation/binary>> ->
+            process_station(NextStation)
     end.
