e08420481fda6affc0bc2c2405aaaf634572c680
Lukas Larsson <lukas@erlang.org>
2024-01-19 08:31:59 +0100

    Polish run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,4 @@ if [ "$2" != "" ]; then
     MODULE="$2"
 fi
 
-# +SDio 1 +SDPcpu 50
-
-erl +sbwt none +sbwtdio none +JPperf true -noinput -s "$MODULE" run "$1" -eval "erlang:halt()."
+erl +SDio 1 +SDPcpu 50 +sbwt none +sbwtdio none -noinput -s "$MODULE" run "$1" -eval "erlang:halt()."
