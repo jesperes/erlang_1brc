1f631d749cf32e4e441a3da18a097729d62e27b0
Jesper Eskilson <jesper.eskilson@klarna.com>
2024-01-24 09:33:16 +0100

    Cleanups

--- src/aggregate.erl
+++ src/aggregate.erl
@@ -3,29 +3,24 @@
 -export([ run/1
         , aggregate_measurements/2
         , chunk_processor/0
-        , line_processor/0
-        , parse_float/1
         ]).
 
--include_lib("eunit/include/eunit.hrl").
+-compile({inline,[{process_temp,2},{process_line,3}]}).
 
 run([Filename]) ->
-%    logger:update_primary_config(#{ level => none }),
-    aggregate_measurements(atom_to_list(Filename), [{bufsize, 2 * 1024 * 1024}]).
+    logger:update_primary_config(#{ level => none }),
+    aggregate_measurements(atom_to_list(Filename), [{bufsize, 2 * 1024 * 1240}]).
 
 aggregate_measurements(Filename, Opts) ->
   process_flag(trap_exit, true),
-  Start = erlang:monotonic_time(),
   BufSize = proplists:get_value(bufsize, Opts),
   logger:info(#{bufsize => BufSize}),
-  {ok, FD} = prim_file:open(Filename, [read]),
+  {ok, FD} = file:open(Filename, [raw, read, binary]),
   NumProcessors =
-    proplists:get_value(parallel, Opts, erlang:system_info(schedulers) div 2),
+    proplists:get_value(parallel, Opts, erlang:system_info(schedulers)),
   {ProcessorPids, AllPids} = start_processors(NumProcessors),
-  read_chunks(FD, 0, 0, <<>>, BufSize, ProcessorPids),
-  Now = erlang:monotonic_time(),
-  logger:info(#{label => "All chunks read, waiting for processors to finish",
-                elapsed_secs => (Now - Start) / 1000_000_000.0}),
+  {ok, Bin} = file:pread(FD, 0, BufSize),
+  read_chunks(FD, 0, byte_size(Bin), Bin, BufSize, ProcessorPids, NumProcessors * 3),
   Map = wait_for_completion(AllPids, #{}),
 
   %% ?assertMatch({_, 180, _, _}, maps:get(<<"Abha">>, Map)),
@@ -37,36 +32,37 @@ aggregate_measurements(Filename, Opts) ->
   end.
 
 start_processors(NumProcs) ->
-  logger:info(#{label => "Starting processing pipelines",
-                num_pipelines => NumProcs}),
   lists:foldl(
     fun(_, {ProcessorPids, AllPids}) ->
-        LineProcessorPid = proc_lib:start_link(?MODULE, line_processor, []),
         ProcessorPid = proc_lib:start_link(?MODULE, chunk_processor, []),
-        ProcessorPid ! {line_processor, LineProcessorPid},
-        LineProcessorPid ! {result_pid, self()},
+        ProcessorPid ! {result_pid, self()},
         {[ProcessorPid|ProcessorPids],
-         [ProcessorPid, LineProcessorPid|AllPids]}
+         [ProcessorPid|AllPids]}
     end, {[], []}, lists:seq(1, NumProcs)).
 
-read_chunks(FD, N, Offset, PrevChunk, BufSize, TargetPids) ->
+read_chunks(FD, N, Offset, PrevChunk, BufSize, TargetPids, 0) ->
+    receive
+        give_me_more ->
+            read_chunks(FD, N, Offset, PrevChunk, BufSize, TargetPids, 1)
+    end;
+read_chunks(FD, N, Offset, PrevChunk, BufSize, TargetPids, Outstanding) ->
   TargetPid = lists:nth((N rem length(TargetPids)) + 1, TargetPids),
-  case prim_file:pread(FD, Offset, BufSize) of
+  case file:pread(FD, Offset, BufSize) of
     {ok, Bin} ->
       Size = byte_size(Bin),
       %% Read chunks pair-wise and split them so that each processed
       %% chunk is on an even newline boundary
       case binary:split(Bin, <<"\n">>) of
         [First, NextChunk] ->
-          send_chunk(<<PrevChunk/binary, First/binary>>, TargetPid),
-          read_chunks(FD, N + 1, Offset + Size, NextChunk, BufSize, TargetPids);
+          send_chunk([PrevChunk, First], TargetPid),
+          read_chunks(FD, N + 1, Offset + Size, NextChunk, BufSize, TargetPids, Outstanding - 1);
         [Chunk] ->
-          send_chunk(Chunk, TargetPid),
-          read_chunks(FD, N + 1, Offset + Size, <<>>, BufSize, TargetPids)
+          send_chunk([Chunk], TargetPid),
+          read_chunks(FD, N + 1, Offset + Size, <<>>, BufSize, TargetPids, Outstanding - 1)
       end;
     eof ->
       %% Reached end of file, process the last chunk
-      send_chunk(PrevChunk, TargetPid),
+      send_chunk([PrevChunk], TargetPid),
       lists:foreach(fun(Pid) -> Pid ! eof end, TargetPids),
       ok
   end.
@@ -75,19 +71,17 @@ send_chunk(Chunk, TargetPid) ->
   TargetPid ! {chunk, Chunk}.
 
 wait_for_completion([], Map) ->
-  logger:info(#{label => "All subprocesses finished"}),
   Map;
 wait_for_completion(Pids, Map) ->
   receive
     {'EXIT', Pid, normal} ->
       wait_for_completion(Pids -- [Pid], Map);
-    {result, SenderPid, NewMap} ->
-      logger:info(#{label => "Got result",
-                    sender => SenderPid,
-                    result_size => maps:size(NewMap)}),
+    {result, _SenderPid, NewMap} ->
       wait_for_completion(Pids, merge_location_data(Map, NewMap));
-    _ ->
-      logger:error(#{label => "Unexpected message"})
+      give_me_more ->
+          wait_for_completion(Pids, Map);
+    M ->
+      logger:error(#{label => "Unexpected message", msg => M})
   end.
 
 merge_location_data(Map1, Map2) ->
@@ -133,97 +127,85 @@ format_final_map(Map) ->
 %%
 chunk_processor() ->
   proc_lib:init_ack(self()),
-  logger:info(#{label => "Chunk processor running"}),
-  chunk_processor_loop(undefined).
+    try
+        chunk_processor_loop(undefined)
+    catch E:R:ST ->
+            logger:error(#{ crashed => {E,R,ST} })
+    end.
 
 chunk_processor_loop(Pid) ->
   receive
-    {line_processor, LineProcessorPid} ->
-      chunk_processor_loop(LineProcessorPid);
-    {chunk, Chunk} ->
-      Pid ! {lines, binary:split(Chunk, [<<"\n">>, <<";">>], [global])},
+    {result_pid, NewPid} ->
+      chunk_processor_loop(NewPid);
+    {chunk, [Chunk]} ->
+      process_station(Chunk),
+          Pid ! give_me_more,
+      chunk_processor_loop(Pid);
+    {chunk, [First, Second]} ->
+          case process_station(First) of
+              <<>> ->
+                  ok;
+              {Rest, Station} ->
+                  process_temp(<<Rest/binary, Second/binary>>, Station);
+              Rest ->
+                  process_station(<<Rest/binary, Second/binary>>)
+          end,
+          Pid ! give_me_more,
       chunk_processor_loop(Pid);
     eof ->
-      logger:info(#{label => "Chunk processor finished."}),
-      Pid ! eof;
-    _ ->
-      logger:error(#{label => "Unexpected message"})
+      Map = maps:from_list(get()),
+      Pid ! {result, self(), Map},
+      ok;
+    M ->
+      io:format("Unexpected message: ~w~n", [M])
   end.
 
 %%
 %% The line processor
 %%
 
-line_processor() ->
-  proc_lib:init_ack(self()),
-  line_processor_loop(undefined).
+process_station(Station) ->
+    process_station(Station, Station, 0).
+process_station(Bin, <<";", Rest/bitstring>>, Cnt) ->
+    <<Station:Cnt/binary, _/bitstring>> = Bin,
+    process_temp(Rest, Station);
+process_station(Bin, <<_:8, Rest/bitstring>>, Cnt) ->
+    process_station(Bin, Rest, Cnt + 1);
+process_station(Bin, _, _Cnt) ->
+    Bin.
 
-line_processor_loop(ResultPid) ->
-  receive
-    {result_pid, Pid} ->
-      line_processor_loop(Pid);
-    {lines, Lines} ->
-      process_lines(Lines),
-      line_processor_loop(ResultPid);
-    eof ->
-      Map = maps:from_list(get()),
-      ResultPid ! {result, self(), Map},
-      {heap_size, HeapSize} = process_info(self(), heap_size),
-      logger:info(#{label => "Line processor finished",
-                    heap_size => HeapSize}),
-      ok;
-    _ ->
-      logger:error(#{label => "Unexpected message"})
-  end.
-
-process_lines([]) ->
-  ok;
-process_lines([<<>>]) ->
-  ok;
-process_lines([Station, TempBin|Rest]) ->
-  Temp = parse_float(TempBin),
-  case get(Station) of
-    undefined ->
-      put(Station, { Temp % min
-                   , Temp % max
-                   , 1    % count
-                   , Temp % sum
-                   });
-
-    {OldMin, OldMax, OldCount, OldSum} ->
-      put(Station, { min(OldMin, Temp)
-                   , max(OldMax, Temp)
-                   , OldCount + 1
-                   , OldSum + Temp
-                   })
-
-  end,
-  process_lines(Rest).
-
-%% Very specialized float-parser for floats with a single fractional
-%% digit, and returns the result as an integer * 10.
 -define(TO_NUM(C), (C - $0)).
 
-parse_float(<<$-, A, B, $., C>>) ->
-  -1 * (?TO_NUM(A) * 100 + ?TO_NUM(B) * 10 + ?TO_NUM(C));
-parse_float(<<$-, B, $., C>>) ->
-  -1 * (?TO_NUM(B) * 10 + ?TO_NUM(C));
-parse_float(<<A, B, $., C>>) ->
-  ?TO_NUM(A) * 100 + ?TO_NUM(B) * 10 + ?TO_NUM(C);
-parse_float(<<B, $., C>>) ->
-  ?TO_NUM(B) * 10 + ?TO_NUM(C).
+process_temp(<<$-, A, B, $., C, Rest/binary>>, Station) ->
+    process_line(Rest, Station, -1 * (?TO_NUM(A) * 100 + ?TO_NUM(B) * 10 + ?TO_NUM(C)));
+process_temp(<<$-, B, $., C, Rest/binary>>, Station) ->
+    process_line(Rest, Station, -1 * (?TO_NUM(B) * 10 + ?TO_NUM(C)));
+process_temp(<<A, B, $., C, Rest/binary>>, Station) ->
+    process_line(Rest, Station, ?TO_NUM(A) * 100 + ?TO_NUM(B) * 10 + ?TO_NUM(C));
+process_temp(<<B, $., C, Rest/binary>>, Station) ->
+    process_line(Rest, Station, ?TO_NUM(B) * 10 + ?TO_NUM(C));
+process_temp(Rest, Station) ->
+    {Rest, Station}.
 
--ifdef(TEST).
+process_line(Rest, Station, Temp) ->
+    case get(Station) of
+        undefined ->
+            put(Station, { Temp % min
+                         , Temp % max
+                         , 1    % count
+                         , Temp % sum
+                         });
 
-parse_float_test() ->
-  lists:foreach(fun({Bin, Exp}) ->
-                    Float = parse_float(Bin),
-                    ?debugVal({Bin, Float}),
-                    ?assert(abs(Exp - Float) =< 0.0001)
-                end,
-                [ {<<"-0.5">>, -5}
-                , {<<"-10.5">>, -105}
-                , {<<"10.5">>, 105}
-                ]).
+        {OldMin, OldMax, OldCount, OldSum} ->
+            put(Station, { min(OldMin, Temp)
+                         , max(OldMax, Temp)
+                         , OldCount + 1
+                         , OldSum + Temp
+                         })
 
--endif.
+    end,
+    case Rest of
+        <<>> -> <<>>;
+        <<"\n",NextStation/binary>> ->
+            process_station(NextStation)
+    end.
--- src/erlang_1brc.erl
+++ src/erlang_1brc.erl
@@ -3,26 +3,14 @@
 -export([main/1]).
 
 options() ->
-  [ {file,      $f, "file",     {string, "measurements.txt"}, "The input file."}
-  , {eprof,     $e, "eprof",    undefined,                    "Run code under eprof."}
-  , {bufsize,   $c, "bufsize",  {integer, 2 * 1024 * 1024},   "Buffer size."}
-  , {log_level, $l, "log_level", {atom, info},                "Log level."}
-  , {no_output, undefined, "no_output", undefined,            "Do not print output to stdout."}
-  , {parallel,  $p, "parallel", integer,                      "Number of parallel processing pipelines."}
-  , {backend, $b, "backend", {atom, aggregate}, "The backend to use"}
+  [ {file,      $f, "file",      {string, "measurements.txt"}, "The input file."}
+  , {eprof,     $e, "eprof",     undefined,                    "Run code under eprof."}
+  , {bufsize,   $c, "bufsize",   {integer, 2 * 1024 * 1024},   "Buffer size."}
+  , {parallel,  $p, "parallel",  integer,                      "Number of parallel processing pipelines."}
   ].
 
 main(Args) ->
-  logger:update_formatter_config(
-    default,
-    #{ legacy_header => true
-     , single_line => false
-     , template => ["== ", time, " ", level, " ", pid, " ==\n", msg, "\n\n"]
-     }),
-
   {ok, {Opts, []}} = getopt:parse(options(), Args),
-  LogLevel = proplists:get_value(log_level, Opts),
-  logger:update_primary_config(#{level => LogLevel}),
 
   Time =
     case proplists:get_value(eprof, Opts) of
@@ -39,9 +27,8 @@ main(Args) ->
         do_main(Opts)
     end,
 
-  logger:info(#{label => "Finished",
-                elapsed_secs => Time / 1_000_000.0}),
-  logger_std_h:filesync(default).
+  io:format("Finished, time = ~w seconds~n",
+            [erlang:convert_time_unit(Time, microsecond, second)]).
 
 do_main(Opts) ->
   Filename = proplists:get_value(file, Opts),
