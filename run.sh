337287399da7180156b4268251b78d725b79459e
Jesper Eskilson <jesper.eskilson@klarna.com>
2024-01-31 22:22:18 +0100

    Do the compilation in setup.sh

--- run.sh
+++ run.sh
@@ -6,10 +6,8 @@ if [ -z "$INPUT" ]; then
 fi
 
 set -e
-# erlc +bin_opt_info -W src/erlang_1brc.erl
-erlc -W src/erlang_1brc.erl || exit 1
 /bin/time -f "Elapsed time: %e seconds (%E) (%P CPU)" \
     erl \
     -noinput \
-    -s erlang_1brc run "$1" \
+    -s erlang_1brc main "$1" \
     -eval "erlang:halt()."
--- setup.sh
+++ setup.sh
@@ -1,1 +1,2 @@
 #!/bin/bash
+erlc -W src/erlang_1brc.erl
